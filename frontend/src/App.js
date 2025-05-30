9b167e559755cc34865ff6472c5f304260e06cd3
Anish1005 <143640454+Anish1005@users.noreply.github.com>
2025-05-30 22:32:43 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signup from './Pages/LoginSignup/Signup';
 import Login from './Pages/LoginSignup/Login';
 import Home from './Pages/Home/Home';
+import OceanQuiz from './Pages/Quiz/OceanQuiz';
+import OceanResults from './Pages/Results/OceanResults';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/quiz" element={<OceanQuiz />} />
+        <Route path="/results" element={<OceanResults />} />
         
       </Routes>
     </Router>
