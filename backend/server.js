85ab1edc5ab00252b38a1dc8b705bb063bb14fd5
sadneya145 <144504571+sadneya145@users.noreply.github.com>
2025-05-31 19:28:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ const crypto = require("crypto");
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://whoweare-mbtioceantest-frontend.onrender.com',
+  credentials: true
+}));
+
 app.use(express.json());
 
 // MongoDB connection
